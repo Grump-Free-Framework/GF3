b6df08b7cfeb8b5abb9080c92cf9b129fa23b71e
GrumpyCrouton <12193403+GrumpyCrouton@users.noreply.github.com>
2022-07-08 23:43:07 -0500

    Security patch
    Prevent leaking config data in case an error log is shown to a regular user.
    it is still recommended to disable error logging in production code.

--- app/controller.php
+++ app/controller.php
@@ -1,7 +1,7 @@
 <?php
 
 class Controller {
-	
+
 	protected $f3;
 
 	function beforeroute(){
@@ -30,15 +30,27 @@ class Controller {
 		//instead just extend the page contoller class with Controller to have access to this variable.
 		//on page controllers base can be accessed by $this->f3
 		$this->f3 = Base::instance();
-		
+
+		//list of values to clear in case of error log dumps. This is to prevent leaking config data in case an error log is shown to a regular user.
+		//it is still recommended to disable error logging in production code.
+		$redact_config_values = [
+		    'db_host',
+		    'db_password',
+		    'db_database',
+		    'db_username'
+		];
+		foreach($redact_config_values as $key) {
+		    $f3->set($key, 'REDACTED');
+		}
+
 		$module = $this->f3->get("PARAMS.module");
 		$this->f3->module = $module ? $module : $this->f3->get("defaultModule");
-		
+
 		$method = $this->f3->get("PARAMS.method");
 		$this->f3->method = !empty($method) ? $method : $this->f3->VERB;
-		
+
 	}
-	
+
 	public function render($content, $template = 'default') {
 		if($template == "default") {
 			$template = $this->f3->get('defaultTemplate');
@@ -47,7 +59,7 @@ class Controller {
 		$this->f3->set('content', "$module/views/$content.htm");
 		return \Template::instance()->render("../$template.htm");
 	}
-	
+
 	public function model($model, $module_override = null) {
 		$module = $module_override === null ? $this->f3->module : $module_override;
 		$class = "\\modules\\{$module}\\models\\{$model}";
